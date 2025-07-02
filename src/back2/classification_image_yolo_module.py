9b31547bd0347e6607c2cc28da22d3fb49bc1dfa
powder1122 <choyk0406@naver.com>
2025-07-02 17:38:24 +0900

    예외처리 추가

--- src/back2/classification_image_yolo_module.py
+++ src/back2/classification_image_yolo_module.py
@@ -8,8 +8,12 @@ class IngredientDetector:
     model_path: str = 모델 파일 경로, 문자열 타입 명시
     '''
     def __init__(self, model_path: str):
-        self.model = YOLO(model_path) # YOLO 모델을 로드
-        self.class_names = self.model.names  # 클래스 ID와 이름 매핑 딕셔너리 (예: {0: 'egg', 1: 'tomato'})
+        #예외처리
+        try:    
+            self.model = YOLO(model_path) # YOLO 모델을 로드
+            self.class_names = self.model.names  # 클래스 ID와 이름 매핑 딕셔너리 (예: {0: 'egg', 1: 'tomato'})
+        except FileNotFoundError as e:
+            print('파일을 찾을 수 없습니다. 모델의 경로를 다시 확인해주세요.')
 
     # 식재료 분류 메서드
     def classify_ingredients(self, image_paths: Union[str, List[str]]) -> List[Dict[str, int]]:
@@ -22,20 +26,22 @@ class IngredientDetector:
         if isinstance(image_paths, str):
             # 단일 문자열이면 리스트로 변환하여 일관된 처리
             image_paths = [image_paths]
-        # 모델을 통한 분류 실행
-        results = self.model(image_paths)
+            
         # 모든 이미지의 감지 결과를 저장할 리스트
         all_labels = []
 
         # 각 이미지에 대한 결과 반복
-        for result in results:
-            labels = [] # 현재 이미지의 식재료를 저장하는 리스트
-            for box in result.boxes:    # 감지된 객체들 반복
-                cls_id = int(box.cls)   # 클래스ID를 정수로 변환(0.0 --> 0 등)
-                label = self.class_names[cls_id]    # 클래스 ID를 이름으로 변환(1 --> 사과 등)
-                labels.append(label)    #이름을 리스트에 추가
-            all_labels.append(list(set(labels)))   # 현재 이미지 결과를 전체 리스트에 추가(중복 제거)
-
+        for i, result in enumerate(self.model(image_paths)):    #오류 이미지의 순서를 알기 위해 추가
+            try:
+                labels = [] # 현재 이미지의 식재료를 저장하는 리스트
+                for box in result.boxes:    # 감지된 객체들 반복
+                    cls_id = int(box.cls)   # 클래스ID를 정수로 변환(0.0 --> 0 등)
+                    label = self.class_names[cls_id]    # 클래스 ID를 이름으로 변환(1 --> 사과 등)
+                    labels.append(label)    #이름을 리스트에 추가
+                all_labels.append(list(set(labels)))   # 현재 이미지 결과를 전체 리스트에 추가(중복 제거)
+            except Exception as e:
+                print(f'!!!에러 발생!!! {i}번째 이미지를 처리 중 오류가 발생했습니다. 오류: {e}')
+                all_labels.append([])   #오류가 난 경우 빈 리스트를 추가 후 계속 진행
         return all_labels
     #JSON 변환 메서드
     def to_json(self, image_paths: Union[str, List[str]]) -> str: 
@@ -43,8 +49,13 @@ class IngredientDetector:
         classify_ingredients 결과를 JSON 형식으로 반환
         반환값: JSON 문자열
         """
-        all_labels = self.classify_ingredients(image_paths) # 감지 결과 가져오기 
-        return json.dumps({"results": all_labels}, ensure_ascii=False, indent=2)    # 한글은 없지만 한글이 안깨지게 설정. 들여쓰기까지(가독성)
+        try:
+            all_labels = self.classify_ingredients(image_paths) # 감지 결과 가져오기 
+            return json.dumps({"results": all_labels}, ensure_ascii=False, indent=2)    # 한글은 없지만 한글이 안깨지게 설정. 들여쓰기까지(가독성)
+        
+        except Exception as e:
+            print(f'to_json 오류 발생: {e}')
+            return json.dumps({"error": str(e)}, ensure_ascii=False, indent=2)
     
 
 #main파일
